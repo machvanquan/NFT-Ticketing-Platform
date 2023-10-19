d63fa8fa7512b884565471312b041b46530b5295
PhuocTDP <137909698+PhuocTDP@users.noreply.github.com>
2023-10-19 15:07:59 +0700

    Update Top.jsx

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Top() {
+function TopCustomers() {
   return (
     <>
       <section className="section hero" aria-label="home">
@@ -10,8 +10,47 @@ function Top() {
           </h1>
         </div>
       </section>
+      
+      <section>
+        <div className="container">
+          <table className="table table-striped table-bordered table-hover">
+            <thead>
+              <tr>
+                <th className="label-control">#</th>
+                <th className="label-control">Photo</th>
+                <th className="label-control">Fullname</th>
+                <th className="label-control">Email</th>
+                <th className="label-control">Count</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="label-control">1</td>
+                <td className="label-control">Mark</td>
+                <td className="label-control">Otto</td>
+                <td className="label-control">@mdo</td>
+                <td className="label-control">10</td>
+              </tr>
+              <tr>
+                <td className="label-control">2</td>
+                <td className="label-control">John</td>
+                <td className="label-control">Doe</td>
+                <td className="label-control">@johndoe</td>
+                <td className="label-control">15</td>
+              </tr>
+              <tr>
+                <td className="label-control">3</td>
+                <td className="label-control">Jane</td>
+                <td className="label-control">Smith</td>
+                <td className="label-control">@janesmith</td>
+                <td className="label-control">8</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
     </>
   )
 }
 
-export default Top
+export default TopCustomers
