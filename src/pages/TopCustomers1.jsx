c7df18a2d9d949e63b8f5852a95c939fc42829db
ngamait <114874935+maiNga0503@users.noreply.github.com>
2023-10-25 09:18:11 +0700

    Update and rename TopCustomers.jsx to TopCustomers1.jsx
    Mai Thị Nga
    Tiêu Đình Phước

---
+++ src/pages/TopCustomers1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TopCustomers from "../components/TopCustomers";
+import Footer from "../components/Footer";
+function TopCustomers1() {
+  return (
+    <>
+    <TopCustomers/>
+    <Footer />
+    </>
+  );
+};
+
+export default TopCustomers1
